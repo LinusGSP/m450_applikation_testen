915ef1c3d3a076a4b30eb7a7fd347b71daa48c91
Linus Schönbächler <linus.schoenbeachler@wiss-edu.ch>
2023-06-01 09:51:04 +0200

    added password check for admin page

--- frontend/src/Admin.js
+++ frontend/src/Admin.js
@@ -1,12 +1,71 @@
-import React from "react"
-import './Styling/Home.css'
+import React from "react";
 
-export default class Admin extends React.Component{
-    constructor(){
-        super()
+import './Styling/Home.css';
+import {Link} from "react-router-dom";
+import LearnSetInfo from "./LearnSetInfo";
+
+export default class Admin extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            learnsets: [],
+            admin: false,
+            password: ""
+        };
     }
-    render(){
-        return <></>
+
+    componentDidMount() {
+        // fetch all learnset data
+        fetch("http://localhost:8080/api/learnset")
+            .then(response => response.json())
+            .then(jsonData => {
+                this.setState({ learnsets: jsonData });
+                console.log(this.state);
+            });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        if (this.state.password === "12345") {
+            this.setState({ admin: true });
+        }
+    };
+
+    handleChange = (event) => {
+        this.setState({ password: event.target.value });
+    };
+
+    render() {
+        // map over every learnset
+        let display_sets = this.state.learnsets.map(e => {
+            return (
+                <LearnSetInfo key={e.id} learnSet={e} classNames="card hover-size" />
+            );
+        });
+
+        let content = this.state.admin ?
+                <>
+                    <p className="center">Wähle ein Lernset aus oder <Link to="/create">erstelle</Link> dein eigenes</p>
+                    <div className="grid-align">
+                        {display_sets}
+                    </div>
+                </>
+            :
+            <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Password:
+                        <input id="password" type="password" value={this.state.password} onChange={this.handleChange} />
+                    </label>
+                    <button type="submit">Submit</button>
+                </form>
+
+        return (
+            <div className="content">
+                <div className="center title">
+                    <h1>Willkommen bei der Admin Page von QuizMe</h1>
+                </div>
+                {content}
+            </div>
+        );
     }
 }
-
